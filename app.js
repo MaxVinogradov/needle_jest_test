5a1b91bc41699a3d7c31910991f82116b4b04e15
Maxim Vinogradov <vinogradov.max97@gmail.com>
2017-12-01 17:28:12 +0200

    Update test URL

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 module.exports = require('needle').request(
-    'get', 'https://data.42matters.com/api/v2.0/ios/apps/lookup.json',
-    {
-        access_token: 'e48ba097019beb8a7967b1c7ff837a1f5842528f',
-        id: 343200656
-    }, function (err, response, body) {
-        console.log(err, response, body)
+    'get', 'http://www.whatarecookies.com/cookietest.asp', {}, function (err, response, body) {
+        console.log(err, response, body);
     });
